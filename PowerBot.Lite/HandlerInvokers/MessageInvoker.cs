d4e9f12fbd4d54f6e5f1a0e6a265eb0b27128f9d
Andrew Witwicki <vitvitskiy3@gmail.com>
2022-06-18 09:21:23 +0200

    Remove wrong code

--- PowerBot.Lite/HandlerInvokers/MessageInvoker.cs
+++ PowerBot.Lite/HandlerInvokers/MessageInvoker.cs
@@ -17,42 +17,6 @@ using Autofac;
 
 namespace PowerBot.Lite.HandlerInvokers
 {
-
-    public class FastMethodInfot
-    {
-        private delegate object ReturnValueDelegate(object instance, object[] arguments);
-        //private delegate object Delegate(object instance, object[] arguments);
-        private delegate void VoidDelegate(object instance, object[] arguments);
-
-        public FastMethodInfot(MethodInfo methodInfo)
-        {
-            var instanceExpression = Expression.Parameter(typeof(object), "instance");
-            var argumentsExpression = Expression.Parameter(typeof(object[]), "arguments");
-            var argumentExpressions = new List<Expression>();
-            var parameterInfos = methodInfo.GetParameters();
-            for (var i = 0; i < parameterInfos.Length; ++i)
-            {
-                var parameterInfo = parameterInfos[i];
-                argumentExpressions.Add(Expression.Convert(Expression.ArrayIndex(argumentsExpression, Expression.Constant(i)), parameterInfo.ParameterType));
-            }
-            var callExpression = Expression.Call(!methodInfo.IsStatic ? Expression.Convert(instanceExpression, methodInfo.ReflectedType) : null, methodInfo, argumentExpressions);
-            if (callExpression.Type == typeof(void))
-            {
-                var voidDelegate = Expression.Lambda<VoidDelegate>(callExpression, instanceExpression, argumentsExpression).Compile();
-                Delegate = (instance, arguments) => { voidDelegate(instance, arguments); return null; };
-            }
-            else
-                Delegate = Expression.Lambda<ReturnValueDelegate>(Expression.Convert(callExpression, typeof(object)), instanceExpression, argumentsExpression).Compile();
-        }
-
-        private ReturnValueDelegate Delegate { get; }
-
-        public object Invoke(object instance, params object[] arguments)
-        {
-            return Delegate(instance, arguments);
-        }
-    }
-
     public static class MessageInvoker
     {
         public static List<HandlerDescriptor> CollectHandlers()
